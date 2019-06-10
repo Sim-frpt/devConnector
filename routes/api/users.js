bf5ab1518e603d253ed24e95734beb401f0c4c73
Sim <simon.fraipont@gmail.com>
2019-06-10 18:08:26 +0200

    add get route for logged user profile

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { check, validationResult } = require("express-validator/check");
 const config = require('config');
+
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
@@ -25,7 +26,7 @@ router.post('/', [
 
   try {
     let user = await User.findOne({ email: email /* Or we can just put email */});
-    if(user) {
+    if (user) {
       return res.status(400).json({ errors: [{ msg: "User already exists" }] });
     }
 
@@ -57,13 +58,13 @@ router.post('/', [
     jwt.sign(
       payload,
       config.get('jwtSecret'),
-      { expiresIn: 360000},
+      { expiresIn: 360000 },
       (err, token) => {
         if (err) throw err;
         res.json({ token });
       }
     );
-  } catch(err) {
+  } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
